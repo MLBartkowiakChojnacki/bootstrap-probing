c0fd656131818953d90bcdc8915a9f4e196c944d
Krzysztof Chojnacki <kchojnacki@edu.cdv.pl>
2023-01-27 20:01:48 +0100

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -27,14 +27,12 @@ fig.subplots_adjust(hspace=.5)
 #Plot top histogram
 ax = fig.add_subplot(2, 1, 1)
 ax = df_uprawa_2021.hist(bins=50)
-#ax.set_xlim(0,800)
 plt.xlabel('Area')
 plt.ylabel('Frequency')
 
 #Plot bottom histogram
 ax2 = fig.add_subplot(2, 1, 2)
 ax2 = df_uprawa_2022.hist(bins=50)
-#ax2.set_xlim(0,800)
 
 plt.xlabel('Area')
 plt.ylabel('Frequency')
@@ -46,16 +44,13 @@ print('Est. area in 2022: {}'.format(df_uprawa_2022.mean()))
 
 area_2021 = []
 for i in range(10000):
-    #np.random.seed(i)
     area_2021.append((resample(df_uprawa_2021)))
-
 
 area_2021 = np.mean(area_2021, axis=1)
 area_2021
 
 area_est_2022 = []
 for i in range(10000):
-    #np.random.seed(i)
     area_est_2022.append((resample(df_uprawa_2022)))
     
 area_est_2022 = np.mean(area_est_2022, axis=1)
@@ -86,7 +81,6 @@ perms_2021 = []
 perms_2022 = []
 
 for i in range(10000):
-    #np.random.seed(i)
     perms_2021.append(resample(combined, n_samples = len(df_uprawa_2021)))
     perms_2022.append(resample(combined, n_samples = len(df_uprawa_2022)))
     
@@ -115,7 +109,33 @@ plt.ylabel('Frequency')
 plt.axvline(obs_difs, color='r')
 plt.show()
 
+if p_value >= 0.05: print('Brak podstaw do odrzucenia H0')
+
+
 #%%
+
+import pandas as pd
+import numpy as np
+from bstrap import bootstrap, boostrapping_CI
+
+# 1. implement metric
+metric = np.mean
+
+# 4. compare method 1 and 2
+stats_method1, stats_method2, p_value = bootstrap(metric, df_uprawa_2021, df_uprawa_2022, nbr_runs=10000)
+print(stats_method1)
+print(stats_method2)
+print(p_value)
+
+# compute CI and mean for each method separately
+stats_method1 = boostrapping_CI(metric, df_uprawa_2021, nbr_runs=1000)
+stats_method2 = boostrapping_CI(metric, df_uprawa_2022, nbr_runs=1000)
+print(stats_method1)
+print(stats_method2)
+
+
+#%%
+
 data_1 = (df_uprawa_2021,)
 data_2 = (df_uprawa_2022,)
 res_1 = bootstrap(data_1, np.mean, confidence_level=0.95, n_resamples = 9999)
@@ -134,13 +154,4 @@ ax.hist(res_2.bootstrap_distribution, bins=20, color='c', edgecolor='k')
 plt.axvline(df["X2b"].mean(), color='k', linestyle='dashed', linewidth=2)
 plt.axvline(res_2.confidence_interval[0], color='r', linestyle='dashed', linewidth=1)
 plt.axvline(res_2.confidence_interval[1], color='r', linestyle='dashed', linewidth=1)
-plt.show()
-
-#%%
-
-dfs = [df_uprawa_2021, df_uprawa_2022]
-
-fig = plt.figure(figsize =(10, 7))
-ax = fig.add_axes([0, 0, 1, 1])
-bp = ax.boxplot(dfs)
 plt.show()
