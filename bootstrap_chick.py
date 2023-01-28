db8008e516ec3b85a2b8a1120a14af9780389982
Krzysztof Chojnacki <kchojnacki@edu.cdv.pl>
2023-01-28 11:13:34 +0100

    2023-01-28

--- bootstrap_chick.py
+++ bootstrap_chick.py
@@ -8,59 +8,76 @@ Created on Fri Jan 27 22:10:26 2023
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from sklearn.utils import resample
+import matplotlib.pyplot as plt
 
 #load data
-df = pd.read_csv('ChickData.csv', sep = ';')
+df = pd.read_csv('Grupa.1.csv', sep = ';')
+
+df_area_2021 = df['X2a']
+df_area_2022 = df['X2b']
+df_area_2021 = df_area_2021.to_frame()
+df_area_2022 = df_area_2022.to_frame()
+df_area_2021['year'] = '2021'
+df_area_2022['year'] = '2022'
+df_area_2021.columns = ['agr_area', 'year']
+df_area_2022.columns = ['agr_area', 'year']
+
+df_all = pd.concat([df_area_2021, df_area_2022])
 
 #plot boxplot
-sns.boxplot(data=df, x="weight", y="feed")
-
-df[df['feed'] == 'meatmeal']['weight'].mean()
-
-#calculate means
-df[df['feed'] == 'casein']['weight'].mean()
-df[df['feed'] == 'meatmeal']['weight'].mean()
+sns.boxplot(data=df_all, x="agr_area", y="year")
 
 #absolute difference in means
-test_stat1 = abs(df[df['feed'] == 'casein']['weight'].mean() - 
-                 df[df['feed'] == 'meatmeal']['weight'].mean())
+test_stat1 = abs(df_area_2021['agr_area'].mean() - 
+                 df_area_2022['agr_area'].mean())
 
-#calculate median
-df[df['feed'] == 'casein']['weight'].median()
-df[df['feed'] == 'meatmeal']['weight'].median()
 
 #absolute difference in medians
-test_stat2 = abs(df[df['feed'] == 'casein']['weight'].median() -
-                 df[df['feed'] == 'meatmeal']['weight'].median())
+test_stat2 = abs(df_area_2021['agr_area'].median() -
+                 df_area_2022['agr_area'].median())
 
-#np.random.seed(112358)
 
-n = df['feed'].size
+n = df_all['year'].size
 B = 10000
-variable = df['weight']
+variable = df_all['agr_area']
 
 bootstrap_samples = np.random.choice(variable, size = n * B).reshape(n, B)
 
 boot_test_stat1 = np.zeros(B)
+boot_test_hist1 = np.zeros(B)
 boot_test_stat2 = np.zeros(B)
+boot_test_hist2 = np.zeros(B)
 
 for i in range(B):
-    boot_test_stat1[i] = abs(np.mean(bootstrap_samples[0:11,i]) - 
-                             np.mean(bootstrap_samples[12:22,i]))
-    boot_test_stat2[i] = abs(np.median(bootstrap_samples[0:11,i]) - 
-                             np.median(bootstrap_samples[12:22,i]))
-    
+    boot_test_hist1[i] = np.mean(bootstrap_samples[0:199,i])
+    boot_test_stat1[i] = abs(np.mean(bootstrap_samples[0:199,i]) - 
+                             np.mean(bootstrap_samples[200:399,i]))
+    boot_test_hist2[i] = np.mean(bootstrap_samples[200:399,i])
+    boot_test_stat2[i] = abs(np.median(bootstrap_samples[0:199,i]) - 
+                             np.median(bootstrap_samples[200:399,i]))
+
+fig, ax = plt.subplots()
+ax.hist(boot_test_hist1, bins=20, color='c', edgecolor='k')
+plt.axvline(df_area_2021['agr_area'].mean(), color='k', linestyle='dashed', linewidth=2)
+plt.axvline(np.percentile(boot_test_hist1, 2.5), color='r', linestyle='dashed', linewidth=1)
+plt.axvline(np.percentile(boot_test_hist1, 97.5), color='r', linestyle='dashed', linewidth=1)
+plt.show()
+
+fig, ax = plt.subplots()
+ax.hist(boot_test_hist2, bins=20, color='c', edgecolor='k')
+plt.axvline(df_area_2022['agr_area'].mean(), color='k', linestyle='dashed', linewidth=2)
+plt.axvline(np.percentile(boot_test_hist2, 2.5), color='r', linestyle='dashed', linewidth=1)
+plt.axvline(np.percentile(boot_test_hist2, 97.5), color='r', linestyle='dashed', linewidth=1)
+plt.show()    
+
 """
 Wartosc p = ilosc statystyk boostrapowych wiekszych niz te obserwowane /
             total ilosc statstyk bootstrapowych
 """
-print(np.mean(boot_test_stat1 >= test_stat1))
+p_value = np.mean(boot_test_stat1 >= test_stat1)
+print(p_value)
 
-"""
-Sposrod 10000 statystyk bootstrapowych 1048 mialo srednia wieksza niz ta
-obserwowana
-Brak podstaw do odrzucenia H0
-"""
+p_value = np.mean(boot_test_stat2 >= test_stat2)
+print(p_value)
 
-print(np.mean(boot_test_stat2 >= test_stat2))
+
